5a8b369c0ed21cc6cdc9095b4a47c48737214e15
inochigakegohan <kishiura6202@gmail.com>
2024-06-12 09:02:13 +0900

    build: bootstrap4を追加

--- mybook/mybook/settings.py
+++ mybook/mybook/settings.py
@@ -28,15 +28,16 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cms.apps.CmsConfig',  # cms アプリケーション
-]
+    'cms.apps.CmsConfig',   # cms アプリケーション
+    'bootstrap4',           # django-bootstrap4
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
